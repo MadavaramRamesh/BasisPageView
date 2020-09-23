77b372fec51cd1bc5f883819a0b16cdda03b6b75
Ramesh Madavaram <madavaramramesh@gmail.com>
2020-09-23 23:11:02 +0530

    Initial Commit

---
+++ BasisCardView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasisCardView
+//
+//  Created by Test on 23/09/20.
+//  Copyright © 2020 Ramesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
